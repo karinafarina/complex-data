a2da3a8618698ab103f9d3f279242cc1fb507baa
Karina Gaulin <karina@domandtom.com>
2019-06-18 15:44:56 -0400

    Finish

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const apiKey = 'XO7bZfNglYYLNPWLfqVIomzDHgcJxCx3yh3PDZUK';
-const searchURL = 'developer.nps.gov/api/v1';
+const apiKey = 'wymxdOSRoePeEwKzIPoGSa31O5AZiBNmK4f9xI4K';
+const searchURL = "https://developer.nps.gov/api/v1/parks";
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -9,22 +9,50 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
+function displayResults(responseJson) {
+  console.log('responseJson: ', responseJson);
+  $('#results-list').empty();
+  for(let i = 0; i < responseJson.data.length; i++) {
+    let data = responseJson.data[i];
+    $('#results-list').append(`<li><p>${data.fullName}</p>
+      <p>${data.description}</p>
+      <a href="${data.url}">website</a>
+      </li>
+      `)
+  };
+  $('#results').removeClass('hidden');
+};
+
 function getParks(stateCode, maxResults=10) {
   const params = {
-    key: apiKey,
-    q: parkName,
+    api_key: apiKey,
+    limit: maxResults,
     stateCode: stateCode
   };
 
-    const queryString = formatQueryParams(params);
-    const url = searchURL + '?' + queryString;
 
-    console.log(url);
+  const queryString = formatQueryParams(params);
+  const url = searchURL + '?' + queryString;
+
+  console.log('url is: ', url);
+
+  fetch(url)
+    .then(response => {
+      console.log('response is: ', response)
+      if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => displayResults(responseJson))
+  .catch(err => {
+    $('#js-error-message').text(`Something went wrong: ${err.message}`);
+  });
 }
 
 
 function watchForm() {
-  ('form').submit(event => {
+  $('form').submit(event => {
     event.preventDefault();
     const stateCode = $('#js-state-code').val();
     const maxResults = $('#js-max-results').val();
